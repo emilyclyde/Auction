673e90d31f05ff6ca2e6682dca3056888888da12
Rob Callahan <Caddisflyrob@yahoo.com>
2015-03-31 22:58:22 -0700

    Added "AuctionDetails" and "Contact" Models Created a HomePage ViewModel

---
+++ Auction/ViewModels/HomeVM.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auction.ViewModels
+{
+    public class HomeVM
+    {
+        public string Theme { get; set; }
+
+        public string Date { get; set; }
+
+        public string Location { get; set; }
+
+        public string Time1 { get; set; }
+
+        public string Time2 { get; set; }
+
+        public string Time3 { get; set; }
+
+        public string DoorCost { get; set; }
+
+        public string EarlyCost { get; set; }
+
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+}
