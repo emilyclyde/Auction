dd0fda666cbcabd1b0100de71adf01bb7b727245
Ryan <migitcheetah@gmail.com>
2015-04-01 14:56:23 -0700

    Added models

---
+++ Auction/Models/AuctionType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Auction.Models
+{
+    public class AuctionType
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
