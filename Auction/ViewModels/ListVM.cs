6e59f451f190278cb999421aea6404823c0c5c7f
Rob Callahan <Caddisflyrob@yahoo.com>
2015-04-04 12:38:53 -0700

    Updated List view Model with a date

---
+++ Auction/ViewModels/ListVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auction.ViewModels
+{
+    public class ListVM
+    {
+        public List<String> ItemList { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string CurrentDate { get; set; }
+    }
+}
