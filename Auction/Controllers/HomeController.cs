308545552edaaf0a5cf485a9eb1f897da3bfdaa5
Rob Callahan <Caddisflyrob@yahoo.com>
2015-04-04 00:15:17 -0700

    Home Page update

--- Auction/Controllers/HomeController.cs
+++ Auction/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Auction.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,25 +7,43 @@ using System.Web.Mvc;
 
 namespace Auction.Controllers
 {
-  public class HomeController : Controller
-  {
-    public ActionResult Index()
+    public class HomeController : Controller
     {
-      return View();
+        //private AuctionContext db = new AuctionContext();
+
+        public ActionResult Index()
+        {
+            HomeVM contents = new HomeVM();
+
+            contents.Theme = "Polynesian Paradise";
+            contents.Date = "May 23, 2015";
+            contents.LocationName = "Emanuel Baptist Church";
+            contents.LocationAddress="3050 Game Farm Rd Springfield, OR";
+            contents.Time1 = "Doors open at 5:30";
+            contents.Time2 = "Buffet Dinner at 6:30";
+            contents.Time2 = "Live Auction at 7:00";
+            contents.DoorCost = "$20.00";
+            contents.EarlyCost = "$15.00";
+            contents.EarlyTicketDate = "May 15";
+            contents.Name = "Debra Norland";
+            contents.Email = "logosacademymusic@gmail.com";
+            contents.PhoneNumber = "(541)747-0702";
+
+            return View(contents);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
     }
-
-    public ActionResult About()
-    {
-      ViewBag.Message = "Your application description page.";
-
-      return View();
-    }
-
-    public ActionResult Contact()
-    {
-      ViewBag.Message = "Your contact page.";
-
-      return View();
-    }
-  }
 }
