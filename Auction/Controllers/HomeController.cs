210f22b5b4d5c4a51163a99d0120694b9c6ad3c8
Rob Callahan <Caddisflyrob@yahoo.com>
2015-04-04 12:31:35 -0700

    Added Item List View

--- Auction/Controllers/HomeController.cs
+++ Auction/Controllers/HomeController.cs
@@ -13,12 +13,13 @@ namespace Auction.Controllers
 
         public ActionResult Index()
         {
+            //temporary hardcoded Data used untill the DB context is created
             HomeVM contents = new HomeVM();
 
             contents.Theme = "Polynesian Paradise";
             contents.Date = "May 23, 2015";
             contents.LocationName = "Emanuel Baptist Church";
-            contents.LocationAddress="3050 Game Farm Rd Springfield, OR";
+            contents.LocationAddress = "3050 Game Farm Rd Springfield, OR";
             contents.Time1 = "Doors open at 5:30";
             contents.Time2 = "Buffet Dinner at 6:30";
             contents.Time2 = "Live Auction at 7:00";
@@ -32,6 +33,32 @@ namespace Auction.Controllers
             return View(contents);
         }
 
+        public ActionResult List()
+        {
+            //temporary hardcoded Data used untill the DB context is created
+            ListVM list = new ListVM();
+            DateTime date = new DateTime();
+            List<string> items = new List<string>(); 
+ 
+            date = DateTime.Now;
+
+            list.Name = "Debra Norland";
+            list.Email = "logosacademymusic@gmail.com";
+            list.PhoneNumber = "(541)747-0702";
+            list.CurrentDate = date.ToString("d");
+
+            items.Add("Sun River 5 Night Getaway");
+            items.Add("Mckenzie River Rafting Adventure");
+            items.Add("NewPort Deep Sea Fishing Excursion");
+            items.Add("Hoodoo Ski Weekend");
+            items.Add("24 Karat Diamond Necklace");
+            items.Add("Cedar Patio Furniture Set");
+
+            list.ItemList = items;
+
+            return View(list);
+
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -45,5 +72,7 @@ namespace Auction.Controllers
 
             return View();
         }
+
+
     }
 }
