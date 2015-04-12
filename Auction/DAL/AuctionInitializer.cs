b6626b13781a727bee9be5b054ac9a1b430b963d
Rob Callahan <Caddisflyrob@yahoo.com>
2015-04-12 15:39:17 -0700

    Added donors

--- Auction/DAL/AuctionInitializer.cs
+++ Auction/DAL/AuctionInitializer.cs
@@ -55,21 +55,24 @@ namespace Auction.DAL
 
       var donors = new List<Donor>
         {
-          new Donor{ID=1, Name="Phil Night", Address="2800 Highview Dr.",  City="Beaverton", State="OR", Zip="97443", Email="MrPhil@Nike.com", },
-          new Donor{},
+          new Donor{ID=1, Name="Phil Night", Address="2800 Highview Dr.",  City="Beaverton", State="OR", Zip="97443", Email="MrPhil@Nike.com", Items="Autographed Duck BasketBall", Value=150 },
+          new Donor{ID=2, Name="Adam Tubbs", Address="19034 West B st.",  City="Springfield", State="OR", Zip="97478", Email="ATubbs54@yahoo,com", Items="Rafting trip", Value=400 },
+          new Donor{ID=2, Name="Albertsons Bakery", Address="5820 Main St.",  City="Springfield", State="OR", Zip="97478", Email= "None", Items="German Chocolate Cake", Value=12.00m },
+          new Donor{ID=2, Name="Flashback Grill", Address="4210 Main St.",  City="Springfield", State="OR", Zip="97478", Email= "FlashbackGrill@Yahoo.com", Items="Gift certificate", Value=30.00m },
+        
         };
 
-      //  donors.ForEach(o => context.Donors.Add(o));
-      //  context.SaveChanges();
+      donors.ForEach(o => context.Donors.Add(o));
+      context.SaveChanges();
 
-      //  var items = new List<Item>
-      //  {
-      //    new Item{},
-      //    new Item{},
-      //  };
+      var items = new List<Item>
+        {
+          new Item{},
+          new Item{},
+        };
 
-      //  items.ForEach(i => context.Items.Add(i));
-      //  context.SaveChanges();
+      items.ForEach(i => context.Items.Add(i));
+      context.SaveChanges();
 
       //  var multiplebidderitems = new List<MultipleBidderItem>
       //  {
