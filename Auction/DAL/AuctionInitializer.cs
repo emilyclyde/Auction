19df07d33b0b4a0a702e54701b4fb657e986c94c
Rob Callahan <Caddisflyrob@yahoo.com>
2015-04-12 16:06:57 -0700

    Added some Item data

--- Auction/DAL/AuctionInitializer.cs
+++ Auction/DAL/AuctionInitializer.cs
@@ -31,14 +31,15 @@ namespace Auction.DAL
       context.SaveChanges();
 
 
-      //  var auctiontypes = new List<AuctionType>
-      //  {
-      //    new AuctionType{},
-      //    new AuctionType{},
-      //  };
+      var auctiontypes = new List<AuctionType>
+        {
+          new AuctionType{ID=1, Name="Live"},
+          new AuctionType{ID=2, Name="Silent"},
+          new AuctionType{ID=3, Name="Multi-Bidder"},
+        };
 
-      //  auctiontypes.ForEach(t => context.AuctionTypes.Add(t));
-      //  context.SaveChanges();
+      auctiontypes.ForEach(t => context.AuctionTypes.Add(t));
+      context.SaveChanges();
 
       var bidders = new List<Bidder>
         {
@@ -57,8 +58,8 @@ namespace Auction.DAL
         {
           new Donor{ID=1, Name="Phil Night", Address="2800 Highview Dr.",  City="Beaverton", State="OR", Zip="97443", Email="MrPhil@Nike.com", Items="Autographed Duck BasketBall", Value=150 },
           new Donor{ID=2, Name="Adam Tubbs", Address="19034 West B st.",  City="Springfield", State="OR", Zip="97478", Email="ATubbs54@yahoo,com", Items="Rafting trip", Value=400 },
-          new Donor{ID=2, Name="Albertsons Bakery", Address="5820 Main St.",  City="Springfield", State="OR", Zip="97478", Email= "None", Items="German Chocolate Cake", Value=12.00m },
-          new Donor{ID=2, Name="Flashback Grill", Address="4210 Main St.",  City="Springfield", State="OR", Zip="97478", Email= "FlashbackGrill@Yahoo.com", Items="Gift certificate", Value=30.00m },
+          new Donor{ID=3, Name="Albertsons Bakery", Address="5820 Main St.",  City="Springfield", State="OR", Zip="97478", Email= "None", Items="German Chocolate Cake", Value=12.00m },
+          new Donor{ID=4, Name="Flashback Grill", Address="4210 Main St.",  City="Springfield", State="OR", Zip="97478", Email= "FlashbackGrill@Yahoo.com", Items="Gift certificate", Value=30.00m },
         
         };
 
@@ -67,8 +68,11 @@ namespace Auction.DAL
 
       var items = new List<Item>
         {
-          new Item{},
-          new Item{},
+          new Item{ID=1, AuctionType=2, Title="SkateWorld", Description="Addmission for two to Skate World"},
+          new Item{ID=2, AuctionType=2, Title="Movie Night Basket", Description="5 Family DVDs, Popcorn, Cookie Mix and Disney Blanket" },
+          new Item{ID=3, AuctionType=1, Title="Cedar Creek Weekend Getaway", Description="2 night stay at a secuded 2 bedroom cabin along Cedar Creek."},
+          new Item{ID=4, AuctionType=1, Title="Flowers for a Year", Description="Humbolt Florists will supply a colorful bouquet of seasonal flowers each month. Great for you home or office."},
+          new Item{ID=5, AuctionType=1, Title="Oregon Coast Salmon fishing", Description="Bill Reynolds Salmon Carters, Florence OR, will take you and 3 guests out for a full day of Pacific salmon fishing. "},
         };
 
       items.ForEach(i => context.Items.Add(i));
