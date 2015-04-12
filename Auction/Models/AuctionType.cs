104412d5888588516d0c66d6385d8cc959af9334
Emily <emily.clyde@yahoo.com>
2015-04-12 12:50:44 -0700

    Data Attributes Added

--- Auction/Models/AuctionType.cs
+++ Auction/Models/AuctionType.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Web;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace Auction.Models
 {
