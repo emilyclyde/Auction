1daada803d04e2e7f2803b768d5f876ef081aa19
Rob Callahan <Caddisflyrob@yahoo.com>
2015-04-21 16:41:23 -0700

    Added bid views

--- Auction/Controllers/FormsController.cs
+++ Auction/Controllers/FormsController.cs
@@ -1,24 +1,62 @@
-﻿using System;
+﻿using Auction.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Auction.Models;
 
 namespace Auction.Controllers
 {
+    
+
     public class FormsController : Controller
     {
-        // GET: Forms
+        private AuctionContext db = new AuctionContext();
+
+//index **************************************************************************************************
         public ActionResult Index()
         {
             return View();
         }
 
 
-// Add Winning Bids *************************************************************************************
-        public ActionResult AddWinningBids()
+// Add Winning Silent Auction Bids *************************************************************************************
+       //GET ***********************************************************************************************************
+        public ActionResult AddWinningBidsSilent()
         {
-            return View();
+            var SilentItems = new List<Item>();
+ 
+            foreach(var i in db.Items)
+            {
+                if(i.AuctionType==2)
+                    SilentItems.Add(i);
+            }
+
+            return View(SilentItems);
         }
-    }
-}
+        
+        //POST *********************************************************************************************************
+
+
+
+
+// Add Winning Live Auction Bids *************************************************************************************
+        public ActionResult AddWinningBidsLive()
+        {
+            var LiveItems = new List<Item>();
+
+            foreach (var i in db.Items)
+            {
+                if (i.AuctionType == 1)
+                    LiveItems.Add(i);
+            }
+
+            return View(LiveItems);
+        }
+
+
+
+
+    }//end class
+}//end Namespace
