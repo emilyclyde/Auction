e4190803200fbca4b2ffb618fcb4ff9f577d29c7
Rob Callahan <Caddisflyrob@yahoo.com>
2015-05-10 23:20:50 -0700

    Added reports Controller

---
+++ Auction/Controllers/ReportsController.cs
@@ -0,0 +1,53 @@
+﻿using Auction.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Auction.Controllers
+{
+    public class ReportsController : Controller
+    {
+        private AuctionContext db = new AuctionContext();
+
+// Index *****************************************************************************************
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+// AllBidders  ***********************************************************************************
+        public ActionResult AllBidders()
+        {
+            return View(db.Bidders.ToList());
+        }
+
+
+
+//Individual Bidders *****************************************************************************
+        public ActionResult IndividualBidder()
+        {
+            return View();
+        }
+
+
+
+//All Donors *************************************************************************************
+
+
+
+
+//Individual Donors ******************************************************************************
+
+
+
+
+
+
+
+
+
+    }
+}
