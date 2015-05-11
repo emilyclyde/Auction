0bf8fbee7ffcc0ed8f2656974b9453005fc9ea89
Rob Callahan <Caddisflyrob@yahoo.com>
2015-05-10 23:31:12 -0700

    Added Contact 3

--- Auction/Models/Bidder.cs
+++ Auction/Models/Bidder.cs
@@ -34,6 +34,9 @@ namespace Auction.Models
     [Display(Name = "Bidder Contact 2")]
     public string BidderContact2 { get; set; }
 
+    [Display(Name = "Bidder Contact 3")]
+    public string BidderContact3 { get; set; }
+
     public List<IndividualMultiBidderItem> MultiItems { get; set; }
   }
 }
