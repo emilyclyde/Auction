88ec8321dc8c57ad0ac161b11b26be050f5427ca
Seth Thies <slasher2012@hotmail.com>
2015-05-11 13:34:32 -0700

    Added individual bidder with a VM

--- Auction/Controllers/ReportsController.cs
+++ Auction/Controllers/ReportsController.cs
@@ -1,5 +1,6 @@
 ﻿using Auction.DAL;
 using Auction.Models;
+using Auction.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -39,7 +40,45 @@ namespace Auction.Controllers
             {
                 return HttpNotFound();
             }
-            return View(bidder);
+            IndividualBidderVM IBVM = new IndividualBidderVM();
+
+            IBVM.BidderName = bidder.BidderName;
+            IBVM.BidderNumber=bidder.BidderNumber;
+            IBVM.BidderContact1 = bidder.BidderContact1;
+            IBVM.BidderContact2 = bidder.BidderContact2;
+            IBVM.BidderContact3 = bidder.BidderContact3;
+
+            IBVM.LiveItems = null;           //clears the list
+            IBVM.LiveTotal=0;
+            IBVM.SilentTotal=0;
+            IBVM.MultiTotal=0;
+
+
+
+            foreach(var li in db.Items)
+                if (li.WinningBidder == IBVM.BidderNumber && li.AuctionType == 1)
+                {
+                    IBVM.LiveItems.Add(li); 
+                    IBVM.LiveTotal += (decimal)li.BidAmount;
+                }
+                        
+            foreach(var si in db.Items)
+                if (si.WinningBidder == IBVM.BidderNumber && si.AuctionType == 2)
+                {
+                    IBVM.LiveItems.Add(si); 
+                    IBVM.SilentTotal += (decimal)si.BidAmount;
+                }
+
+            foreach(var mi in db.IndividualMultiBidderItems)
+                if (mi.Bidder_ID == bidder.BidderNumber)
+                {
+                    IBVM.MultiItems.Add(mi);
+                    IBVM.MultiTotal+= (decimal)mi.BidAmount;
+                }
+
+            IBVM.ALLTotal = IBVM.LiveTotal + IBVM.SilentTotal + IBVM.MultiTotal;
+
+            return View(IBVM);
         }
        
 
