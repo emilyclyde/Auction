f073eb19034894a07649aaa4e8c1fe2c6aa3a59f
Rob Callahan <Caddisflyrob@yahoo.com>
2015-05-23 20:48:05 -0700

    Added Tickets model to hold the count and price of tickets

---
+++ Auction/Models/Tickets.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Auction.Models
+{
+    public class Tickets
+    {
+        public int NumEarlyTickets { get; set; }
+
+        public int NumDoorTickets { get; set; }
+
+        public decimal CostEarlyTickets { get; set; }
+
+        public decimal CostDoorTickets { get; set; }
+    }
+}
