2a10ceee000928dc018d42fd8f16e20ae539f394
Rob Callahan <Caddisflyrob@yahoo.com>
2015-05-23 22:39:24 -0700

    Added CRUD Screens for Tickets and added the Edit link in the forms list

---
+++ Auction/Controllers/TicketsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Auction.DAL;
+using Auction.Models;
+
+namespace Auction.Controllers
+{
+    public class TicketsController : Controller
+    {
+        private AuctionContext db = new AuctionContext();
+
+        // INDEX ****************************************************************************************************************
+        public ActionResult Index()
+        {
+            return View(db.Tickets.ToList());
+        }
+
+
+        //Details ***************************************************************************************************************
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tickets tickets = db.Tickets.Find(id);
+            if (tickets == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tickets);
+        }
+
+        //Create ***************************************************************************************************************
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tickets/Create*************************************************************************************************
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,NumEarlyTickets,NumDoorTickets,CostEarlyTickets,CostDoorTickets")] Tickets tickets)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tickets.Add(tickets);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tickets);
+        }
+
+
+
+        //Edit *****************************************************************************************************************
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tickets tickets = db.Tickets.Find(id);
+            if (tickets == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tickets);
+        }
+
+        // POST: Tickets/Edit****************************************************************************************************
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,NumEarlyTickets,NumDoorTickets,CostEarlyTickets,CostDoorTickets")] Tickets tickets)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tickets).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index","forms");
+            }
+            return View(tickets);
+        }
+
+
+
+
+        // Delete ***************************************************************************************************************
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tickets tickets = db.Tickets.Find(id);
+            if (tickets == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tickets);
+        }
+
+        // POST: Tickets/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Tickets tickets = db.Tickets.Find(id);
+            db.Tickets.Remove(tickets);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        //Dispose ***************************************************************************************************************
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
